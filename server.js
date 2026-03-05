9f6a2d467d061f3ab5c5e2a436907f931d217097
Fransuelton <elton6103@gmail.com>
2026-03-04 22:20:04 -0300

    feat: update chat message handling to accept structured data and improve routing

--- server.js
+++ server.js
@@ -7,18 +7,26 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+// Rota do chat (principal)
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// Rota de login
+app.get("/login", (req, res) => {
+  res.sendFile(join(__dirname, "pages/login.html"));
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+  
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  // Agora esperamos um objeto: { user: 'nome', text: 'mensagem' }
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
   });
   
 });
